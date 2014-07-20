43fc9d006b8f3f56ac7f3fbe93a19912badd0522
opi <opi@no-log.org>
2014-07-20 15:28:50 +0200

    [enh] Use LDAP auth, fix folders permissions.

--- sources/conf/dokuwiki.php
+++ sources/conf/dokuwiki.php
@@ -52,13 +52,14 @@ $conf['sneaky_index']= 0;                 //check for namespace read permission 
 $conf['hidepages']   = '';                //Regexp for pages to be skipped from RSS, Search and Recent Changes
 
 /* Authentication Settings */
-$conf['useacl']      = 0;                //Use Access Control Lists to restrict access?
+$conf['useacl']      = 1;                //Use Access Control Lists to restrict access?
+$conf['openregister']= 0;
 $conf['autopasswd']  = 1;                //autogenerate passwords and email them to user
-$conf['authtype']    = 'authplain';      //which authentication backend should be used
-$conf['passcrypt']   = 'smd5';           //Used crypt method (smd5,md5,sha1,ssha,crypt,mysql,my411)
+$conf['authtype']    = 'authldap';      //which authentication backend should be used
+$conf['passcrypt']   = 'sha1';           //Used crypt method (smd5,md5,sha1,ssha,crypt,mysql,my411)
 $conf['defaultgroup']= 'user';           //Default groups new Users are added to
-$conf['superuser']   = '!!not set!!';    //The admin can be user or @group or comma separated list user1,@group1,user2
-$conf['manager']     = '!!not set!!';    //The manager can be user or @group or comma separated list user1,@group1,user2
+$conf['superuser']   = 'YNH_ADMIN_USER';    //The admin can be user or @group or comma separated list user1,@group1,user2
+$conf['manager']     = 'YNH_ADMIN_USER';    //The manager can be user or @group or comma separated list user1,@group1,user2
 $conf['profileconfirm'] = 1;             //Require current password to confirm changes to user profile
 $conf['rememberme'] = 1;                 //Enable/disable remember me on login
 $conf['disableactions'] = '';            //comma separated list of actions to disable
@@ -66,6 +67,17 @@ $conf['auth_security_timeout'] = 900;    //time (seconds) auth data is considere
 $conf['securecookie'] = 1;               //never send HTTPS cookies via HTTP
 $conf['remote']      = 0;                //Enable/disable remote interfaces
 $conf['remoteuser']  = '!!not set !!';   //user/groups that have access to remote interface (comma separated)
+
+/* LDAP Yunohost config */
+$conf['auth']['ldap']['server']      = 'localhost';
+$conf['auth']['ldap']['port']        = 389;
+$conf['auth']['ldap']['version']    = 3;
+$conf['auth']['ldap']['usertree']    = 'ou=users,dc=yunohost,dc=org';
+$conf['auth']['ldap']['userfilter']  = '(&(uid=%{user})(objectClass=posixAccount))';
+# no groups
+#$conf['auth']['ldap']['grouptree']   = 'ou=Group, dc=server, dc=tld';
+#$conf['auth']['ldap']['groupfilter'] = '(&(objectClass=posixGroup)(|(gidNumber=%{gid})(memberUID=%{user})))'; 
+
 
 /* Antispam Features */
 $conf['usewordblock']= 1;                //block spam based on words? 0|1
