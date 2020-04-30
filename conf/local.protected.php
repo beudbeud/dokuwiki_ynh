ac50e4c71938bdbb1341fb3ba8079dbea74e4910
Gofannon <gofannon+github@riseup.net>
2020-04-30 17:41:56 +0200

    Implement new permissions system in YNH 3.7

--- conf/local.protected.php
+++ conf/local.protected.php
@@ -14,15 +14,19 @@
 $conf['useacl']      = 1;                //Use Access Control Lists to restrict access?
 $conf['authtype']    = 'authldap';       //which authentication backend should be used
 $conf['passcrypt']   = 'sha1';           //Used crypt method (smd5,md5,sha1,ssha,crypt,mysql,my411)
-$conf['superuser']   = '__YNH_ADMIN_USER__'; //The admin can be user or @group or comma separated list user1,@group1,user2
-$conf['manager']     = '__YNH_ADMIN_USER__'; //The manager can be user or @group or comma separated list user1,@group1,user2
+$conf['superuser']   = '@__APP__.admin'; //The admin can be user or @group or comma separated list user1,@group1,user2
+$conf['manager']     = '@__APP__.admin'; //The manager can be user or @group or comma separated list user1,@group1,user2
 
 /* LDAP Yunohost config */
 $conf['plugin']['authldap']['server']      = 'localhost';
 $conf['plugin']['authldap']['port']        = 389;
 $conf['plugin']['authldap']['version']     = 3;
 $conf['plugin']['authldap']['usertree']    = 'ou=users,dc=yunohost,dc=org';
-$conf['plugin']['authldap']['userfilter']  = '(&(uid=%{user})(objectClass=posixAccount))';
+$conf['plugin']['authldap']['grouptree']   = 'ou=permission,dc=yunohost,dc=org';
+$conf['plugin']['authldap']['userfilter']  = '(&(objectClass=posixAccount)(uid=%{user})(permission=cn=__APP__.main,ou=permission,dc=yunohost,dc=org))';
+$conf['plugin']['authldap']['groupfilter'] = '(&(objectClass=posixGroup)(memberUID=%{user})(cn=__APP__.*))';
+#$conf['plugin']['authldap']['debug']      = 1;
+
 
 /* Advanced Settings */
 $conf['updatecheck'] = 0;                //automatically check for new releases?
